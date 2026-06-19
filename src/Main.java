189a6ae5e411b3aba713a34743d24eb709a9eb1f
antoineginsburg <antoine.ginsburg@gmail.com>
2026-06-19 10:28:03 +0200

    AG : check de manière aléatoire si l'email est renseigné

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
+import java.util.Random;
+import java.util.random.RandomGenerator;
+
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello, World!");
+        Integer param = RandomGenerator.getDefault().nextInt();
+        boolean emailUserPresent = verificationEmail(param);
+    }
 
+    private static boolean verificationEmail(Integer param) {
+        return param > 500;
     }
 }
+
